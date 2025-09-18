a9fbf29235b69831d4697753a24788bf5a77133f
Bitetti Nicola <T622279@ubspartner.onmicrosoft.com>
2025-09-18 20:05:02 +0200

    typography

--- Solution5/backend/function_llm_proxy/__init__.py
+++ Solution5/backend/function_llm_proxy/__init__.py
@@ -1,7 +1,11 @@
 # function_app.py
-from azure.functions import AsgiFunctionApp, AuthLevel
+
+import azure.functions as func 
 from function_llm_proxy.app.fastapi_app import create_fastapi_app
 
-fastapi_app = create_fastapi_app()
+fast_app = create_fastapi_app()
 # Expose the FastAPI app as an Azure Functions HTTP trigger
-app = AsgiFunctionApp(app=fastapi_app, http_auth_level=AuthLevel.ANONYMOUS)
+# app = AsgiFunctionApp(app=fastapi_app, http_auth_level=AuthLevel.ANONYMOUS)
+
+async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    return await func.AsgiMiddleware(fast_app).handle_async(req, context)
